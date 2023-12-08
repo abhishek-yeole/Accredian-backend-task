4df0ba9ac2bc30a3caf031aa01c7618500ba3452
abhishek-yeole <136891500+abhishek-yeole@users.noreply.github.com>
2023-12-09 01:49:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 const cors = require('cors');
